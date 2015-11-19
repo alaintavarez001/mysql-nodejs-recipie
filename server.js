ead1c085de049d0864b03fd6f5a32dd368f9d879
alaintavarez001 <alain.tavarez001@gmail.com>
2015-11-18 21:48:31 -0500

    made changes
    getting better

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ var connection = mysql.createConnection({
 });
    
 var port = 9000;
-var exoress = require('expires');
+var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 
 //purpose if this is to enable cross domain requesrs
 app.use(function(req, res, next){
@@ -16,9 +17,36 @@ app.use(function(req, res, next){
 	next();
 });
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get('/transactions', function(req,res){
-	
+	connection.query("SELECT * FROM transactions", function (err, rows){
+        if (!err){
+            res.setHeader("content-Type", "application/json");
+            res.end(JSON.stringify(rows));//Important! is is how you actually
+                                          //send back a response
+        }else{
+            console.log("there was an error in the query");
+        }
+    });
+});
+
+app.post('/transactions/:sid/:donation',function (req,res){
+    var sid = req.params.sid;
+    var donation = req.params.donation;
+    connection.query("INSERT INTO transactions (sid, donation) VALUES"+
+                     " ("+sid+","+donation+")",function(error){
+        if (error){
+            console.log("DB insertion failed: "+error);
+            res.end("DB error");
+        }
+        else{
+            console.log("DB insertion succeeded");
+            res.end("Success");
+        }
+    });
 });
 
 app.listen(port);
 console.log("Server listening on port "+port);
+
